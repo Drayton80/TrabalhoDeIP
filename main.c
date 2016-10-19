72954b4fc8c1a558917994e5f767c2b68fd20734
DeCarvalho83 <DeCarvalho83@users.noreply.github.com>
2016-10-19 14:35:43 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+Introducao(){
+
+    printf("\t\t**************************\n");
+    printf("\t\t*    Seja Bem Vindo Ao   *\n");
+    printf("\t\t*     Trabalho Final     *\n");
+    printf("\t\t*     Do Curso de IP     *\n");
+    printf("\t\t* ---------------------- *\n");
+    printf("\t\t*     Prof: Lincoln      *\n");
+    printf("\t\t* ---------------------- *\n");
+    printf("\t\t*   Membros do Projeto   *\n");
+    printf("\t\t**************************\n");
+
+    return 1;
+
+}
+main()
+{
+    Introducao();
+    return 0;
+}
---
+++ personagem.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void WarriorClass(){
+    int life = 100,
+        mana = 10,
+        atkP = 10,
+        defP = 10,
+        atkM = 3,
+        defM = 3,
+        stamina,
+        experience,
+}
+
+void MageClass(){
+    int life = 50,
+        mana = 120,
+        atkP = 3,
+        defP = 3,
+        atkM = 10,
+        defM = 10,
+        stamina,
+        experience,
+}
