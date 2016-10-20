ccd26eaf319fc42f6490c177681e10c614507f83
DeCarvalho83 <DeCarvalho83@users.noreply.github.com>
2016-10-20 09:55:39 -0300

    Update personagem.c

--- personagem.c
+++ personagem.c
@@ -1,24 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void baseClass(){
+    int life,
+        mana,
+        atkP,
+        defP,
+        atkM,
+        defM,
+        staminaI,
+        staminaA,
+        experienceI,
+        experienceA,
+        experienceF,
+}
+
 void WarriorClass(){
-    int life = 100,
-        mana = 10,
-        atkP = 10,
-        defP = 10,
-        atkM = 3,
-        defM = 3,
-        stamina,
-        experience,
 }
 
 void MageClass(){
-    int life = 50,
-        mana = 120,
-        atkP = 3,
-        defP = 3,
-        atkM = 10,
-        defM = 10,
-        stamina,
-        experience,
 }
